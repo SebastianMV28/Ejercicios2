1c4b0231a762fe8b56cd66ee9524e44388c1f40b
SebasttianM <sebasttianvmarin@gmail.com>
2023-08-22 18:24:08 -0500

    correction

--- Ejercicios2/Program.cs
+++ Ejercicios2/Program.cs
@@ -4,13 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int n1, n2, result;
+            const double porcent = 2.99;
+            int n1, n2;
+            double result;
             Console.WriteLine("Enter first number");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter second number");
             n2 = int.Parse(Console.ReadLine());
 
-            result = (n1*n2)*2;
+            result = (n1*n2)*porcent;
             Console.WriteLine("The result of your count is " + result);
 
         }
